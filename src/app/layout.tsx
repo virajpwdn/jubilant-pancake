c1d52e4d9608c9c70870d447c74498b5b863933e
Aniket <aniketvishwakarma2004@gmail.com>
2025-07-07 18:28:18 +0530

    feat(analytics): add umami analytics with better structure and with integration of events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,21 +16,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const umamiSrc = process.env.NEXT_PUBLIC_UMAMI_SRC;
-  const umamiId = process.env.NEXT_PUBLIC_UMAMI_ID;
   return (
     <ViewTransitions>
       <html lang="en">
-        <head>
-          {umamiSrc && umamiId && (
-            <Script
-              src={umamiSrc}
-              data-website-id={umamiId}
-              strategy="afterInteractive"
-              defer
-            />
-          )}
-        </head>
         <body className={`font-hanken-grotesk antialiased`}>
           <ReactLenis root>
             <Navbar />
