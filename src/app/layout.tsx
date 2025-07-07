4a8f8ddde5003513802efaf897e11603b8efe6cf
piyushdhoka <piyushdhoka007@gmail.com>
2025-07-07 16:38:21 +0530

    code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Quote } from '@/components/common/Quote';
 import { generateMetadata as getMetadata } from '@/config/Meta';
 import ReactLenis from 'lenis/react';
 import { ViewTransitions } from 'next-view-transitions';
+import Script from 'next/script';
 
 import './globals.css';
 
@@ -18,6 +19,14 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en">
+        <head>
+          <Script
+            src="https://cloud.umami.is/script.js"
+            data-website-id="df3998a2-e0aa-4727-8b75-6af6b3834047"
+            strategy="afterInteractive"
+            defer
+          />
+        </head>
         <body className={`font-hanken-grotesk antialiased`}>
           <ReactLenis root>
             <Navbar />
